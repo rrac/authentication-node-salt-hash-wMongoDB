da2a073477e9ef105c4f00a7b41dde21acc85566
rrac <ron.connal@gmail.com>
2017-09-13 12:37:53 -0700

    Adding digest for crypto.pbkdf2 (SHA1) in a second location

--- authetication-salt-hash-wMongoDB.js
+++ authetication-salt-hash-wMongoDB.js
@@ -22,7 +22,7 @@ function authenticateUser(id, password, callback)
 			{
 				if (result)
 				{
-					crypto.pbkdf2(password, result.salt.read(0, result.salt.length()), hashIterations, keyLengthBytes, function(err, key){
+					crypto.pbkdf2(password, result.salt.read(0, result.salt.length()), hashIterations, keyLengthBytes, 'sha1', function(err, key){
 						if (err)
 						{
 							console.log(err.message);
@@ -68,7 +68,7 @@ function createUser(doc, callback)
 				doc.salt = new Binary(buf);  //put salt result in a mongodb Binary object
 				
 				//Invoke hash function with salt object
-				crypto.pbkdf2(doc.password, doc.salt.read(0, doc.salt.length()), hashIterations, keyLengthBytes,'sha1', function(err2, key){ 
+				crypto.pbkdf2(doc.password, doc.salt.read(0, doc.salt.length()), hashIterations, keyLengthBytes, 'sha1', function(err2, key){ 
 					if (err2)
 					{
 						console.log(err2.message);
